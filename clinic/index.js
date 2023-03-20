ded7d8f1e8e12d5bd203569c78289c4dd444861c
CthulhuMother <79160791+CthulhuMother@users.noreply.github.com>
2023-03-20 17:08:38 +0300

    Add files via upload

---
+++ clinic/index.js
@@ -0,0 +1,5 @@
+$('.slideshow').slick({
+    prevArrow:"<button type='button' class='slick-prev pull-left'><img src='icons/left.svg' alt='1'></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><img src='icons/right.svg' alt='2'></button>", 
+    dots: true
+});
